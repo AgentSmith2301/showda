9cc318bd620fd3c46d589fddf20af8800fc6b117
Tamerlan <tamerlan346@gmail.com>
2025-09-04 20:08:10 +0300

    сorrected the choice of host

--- src/auth-module/managers/nodemailer-managers.ts
+++ src/auth-module/managers/nodemailer-managers.ts
@@ -5,7 +5,7 @@ export const nodemailer_Managers = {
     // вернуть нужный путь с кодом
     determine_The_Path(host: string, code: string): string {
         const baseUrl = host.split(':');
-        if(baseUrl[0] === 'localhost' || '127.0.0.1') { // host === 'localhost:3003'
+        if(baseUrl[0] === 'localhost' || baseUrl[0] === '127.0.0.1') { // host === 'localhost:3003'
             return `http://localhost:3003/auth/frontend/check-email?code=${code}`;
         } else {
             return `https://showda.vercel.app/auth/frontend/check-email?code=${code}`;
