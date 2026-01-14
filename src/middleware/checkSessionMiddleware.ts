afacd0dbb852c22cb2b13c86d79496690c6ed87f
Tamerlan <tamerlan346@gmail.com>
2026-01-14 16:33:42 +0300

    Fixed the security markup, a bug with the token

--- src/middleware/checkSessionMiddleware.ts
+++ src/middleware/checkSessionMiddleware.ts
@@ -5,8 +5,6 @@ import {authRepoMethods} from '../auth-module/repositories/auth-repositories'
 export async function sessionMiddleware(req: Request, res: Response, next: NextFunction) {
     const info = await authRepoMethods.getSessionsInfo(req.tokenPayload.userId, req.tokenPayload.deviceId);
     const vercionToken = new Date(req.tokenPayload.iat * 1000).toISOString();
-    console.log(vercionToken, ' < == vercionToken')
-    console.log(info,' < == comin token')
     if(info!.iat != new Date(vercionToken)) {
         res.sendStatus(401)
         return
--- src/securityDevices/router/securityDevicesRouter.ts
+++ src/securityDevices/router/securityDevicesRouter.ts
@@ -1,11 +1,12 @@
 import {Router} from 'express';
 import {checkRefreshToken} from '../../middleware/checkTokenMiddleware';
+import { sessionMiddleware } from '../../middleware/checkSessionMiddleware';
 import {securityControllers} from '../controllers/securityControllers'
 
 export const securityRouter = Router();
 securityRouter.get('/devices', checkRefreshToken, securityControllers.getAllActiveSessionsForUser)
 securityRouter.delete('/devices', checkRefreshToken, securityControllers.deleteAllOtherSessions)
-securityRouter.delete('/devices/:deviceId', checkRefreshToken, securityControllers.closeSession)
+securityRouter.delete('/devices/:deviceId', checkRefreshToken, sessionMiddleware, securityControllers.closeSession)
 
 
 
