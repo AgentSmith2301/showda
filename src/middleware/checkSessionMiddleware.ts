0301ad1c8b7d99d8058d7b95975d14c377834f9d
Tamerlan <tamerlan346@gmail.com>
2026-01-14 20:04:33 +0300

    fixed iat comparison

--- src/middleware/checkSessionMiddleware.ts
+++ src/middleware/checkSessionMiddleware.ts
@@ -4,8 +4,12 @@ import {authRepoMethods} from '../auth-module/repositories/auth-repositories'
 
 export async function sessionMiddleware(req: Request, res: Response, next: NextFunction) {
     const info = await authRepoMethods.getSessionsInfo(req.tokenPayload.userId, req.tokenPayload.deviceId);
-    const vercionToken = new Date(req.tokenPayload.iat * 1000).toISOString();
-    if(info!.iat != new Date(vercionToken)) {
+    const vercionToken = new Date(req.tokenPayload.iat * 1000); 
+    // сравнение работает но лучше делать через info!.iat.getTime() !== req.tokenPayload.iat * 1000
+    // в таком случае сравниваются числа
+    // и это быстрее 
+    // или даже через +info!.iat !== vercionToken.valueOf()
+    if(info!.iat.toISOString() !== vercionToken.toISOString()) {
         res.sendStatus(401)
         return
     }
