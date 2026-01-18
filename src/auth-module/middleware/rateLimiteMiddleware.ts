1f39e8b5b19dbc111cc6a942eff38ad0200b09e8
Tamerlan <tamerlan346@gmail.com>
2026-01-18 21:33:26 +0300

    new steps

--- src/auth-module/middleware/rateLimiteMiddleware.ts
+++ src/auth-module/middleware/rateLimiteMiddleware.ts
@@ -16,7 +16,7 @@ export async function rateLimiteMiddleware(req: Request, res: Response, next: Ne
         return
     } 
 
-    await rateLimiteRepositories.create_Url_Info(ip , url, dateNow, req.body) //TODO удалить после получения данных body
+    await rateLimiteRepositories.create_Url_Info(ip , url, dateNow)
     next()
     return
 }
--- src/auth-module/repositories/rateLimiteRepositories.ts
+++ src/auth-module/repositories/rateLimiteRepositories.ts
@@ -2,8 +2,8 @@ import {apiRequestsCollection } from "../../db/mongoDb";
 import { API_Info } from "../types/auth-type";
 
 export const rateLimiteRepositories = {
-    async create_Url_Info(ip: string, url: string, dateTime: Date, body: any ): Promise<void> { //TODO удалить после получения данных body
-        await apiRequestsCollection.insertOne({IP: ip, URL: url, date: dateTime, body}) //TODO удалить после получения данных body
+    async create_Url_Info(ip: string, url: string, dateTime: Date): Promise<void> {
+        await apiRequestsCollection.insertOne({IP: ip, URL: url, date: dateTime})
 
     },
 
