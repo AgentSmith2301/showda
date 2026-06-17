67b6411bf6251845c0bd1a7182c3e87d64b10de9
Tamerlan <tamerlan346@gmail.com>
2026-06-17 18:50:46 +0300

    chenge routs from comment

--- src/comments-module/routers/comments-router.ts
+++ src/comments-module/routers/comments-router.ts
@@ -7,10 +7,10 @@ import {container} from '../../composition-root';
 const commentsController = container.get(CommentsController); // получаем экземпляр CommentsController из контейнера Inversify
 
 export const commentsRouter = Router();
+commentsRouter.put('/:commentId/like-status', bearerAuthorization, objectValidateMetods.likeStatusValidator, commentsController.likeUnlikeCommentController.bind(commentsController))
 commentsRouter.put('/:commentId', bearerAuthorization, objectValidateMetods.updateCommentsValidator, commentsController.updateCommentController.bind(commentsController))
 commentsRouter.get('/:id', getUserInformationFromToken, commentsController.getCommentByIdController.bind(commentsController))
 commentsRouter.delete('/:commentId', bearerAuthorization, commentsController.deleteCommentByIdController.bind(commentsController))
-commentsRouter.put('/:commentId/like-status', bearerAuthorization, objectValidateMetods.likeStatusValidator, commentsController.likeUnlikeCommentController.bind(commentsController))
 
 
 
